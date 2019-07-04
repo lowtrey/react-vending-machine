839b75f979e73d39464525507c8a7b62bdfa27e2
Q. Price <priceqp@gmail.com>
2019-07-04 18:15:26 -0500

    Create initial home page layout

---
+++ src/VendingMachine.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class VendingMachine extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>HELLO I AM A VENDING MACHINE. WHAT WOULD YOU LIKE TO EAT?</h1>
+        <div>
+          <h1>SODA</h1>
+          <h1>CHIPS</h1>
+          <h1>FRESH SARDINES</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VendingMachine;
