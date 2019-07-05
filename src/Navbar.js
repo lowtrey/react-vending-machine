fe0b2d4185f8d723992d82f2ace72d081809bdb2
Q. Price <priceqp@gmail.com>
2019-07-04 20:46:49 -0500

    Add different styling to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 
 class Navbar extends React.Component {
   render() {
     return(
       <div className='Navbar'>
-        <NavLink exact to='/' activeClassName='active'><h1>Home</h1></NavLink>
-        <NavLink exact to='/soda' activeClassName='active'><h1>Soda</h1></NavLink>
-        <NavLink exact to='/chips' activeClassName='active'><h1>Chips</h1></NavLink>
-        <NavLink exact to='/sardines' activeClassName='active'><h1>Sardines</h1></NavLink>
+        <NavLink exact to='/' activeClassName='Navbar-active'><h1>Home</h1></NavLink>
+        <NavLink exact to='/soda' activeClassName='Navbar-active'><h1>Soda</h1></NavLink>
+        <NavLink exact to='/chips' activeClassName='Navbar-active'><h1>Chips</h1></NavLink>
+        <NavLink exact to='/sardines' activeClassName='Navbar-active'><h1>Sardines</h1></NavLink>
       </div>
     );
   }
