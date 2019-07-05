430f365f683d1be482c4d8f029f4e1a515dcb3ee
Q. Price <priceqp@gmail.com>
2019-07-04 20:58:45 -0500

    Add Message styling component and added it to VendingMachine Component

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Message from './Message';
 
 class VendingMachine extends React.Component {
   render() {
     return(
       <div>
-        <div>
+        <Message>
           <h1>HELLO I AM A VENDING MACHINE. WHAT WOULD YOU LIKE TO EAT?</h1>
-        </div>
-        <div>
+        </Message>
+        <Message>
           <Link exact to='/soda'><h1>Soda</h1></Link>
           <Link exact to='/chips'><h1>Chips</h1></Link>
           <Link exact to='/sardines'><h1>Sardines</h1></Link>
-        </div>
+        </Message>
       </div>
     );
   }
